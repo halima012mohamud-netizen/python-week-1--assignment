c7818805e97ec6cf47a7accd1822a58579fd819c
halima012mohamud-netizen <halima012mohamud@gmail.com>
2026-09-23 04:58:46 -0700

    Add introduction program with personal details

---
+++ introduction.py
@@ -0,0 +1,11 @@
+# This program introduces me and my goal for the course.
+
+name = "Adii"
+age = 22
+favourite_language = "Python"
+goal = "To improve my programming skills and become a software developer."
+
+print("My name is", name)
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language)
+print("My goal for this course is", goal)
